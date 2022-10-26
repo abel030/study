d3d9f66fd2e21be2e4cedcf7e88975011a7d4b34
원치훈 <todmlrlxjf@gmail.com>
2022-10-26 19:07:28 +0900

    test

---
+++ Else & Elif Recap .py
@@ -0,0 +1,7 @@
+winner =10
+if winner ==10:
+    print("if")
+elif winner <=25:
+    print("elif")
+else:
+    print("else") 
---
+++ Python Standard Library.py
@@ -0,0 +1,19 @@
+
+from random import randint ## 모듈에서랜덤함수 가져오기.
+
+print ("welcome tp 카지노!")
+user_choice =int(input("choose number!"))
+pc_choice = randint(1,50) ## i imported this
+## 내장함수! 
+
+playing =True
+
+while playing:
+    user_choice =int(input("choose number!"))
+    if user_choice== pc_choice:
+        print("니가 이겼다")
+        playing =False
+    elif user_choice> pc_choice:
+        print("더 낮게! ")
+    elif user_choice<pc_choice:
+        print("더 높게! ")
