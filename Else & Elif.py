bee06508ccab562ca788b673935bd9ca0cf17c87
원치훈 <todmlrlxjf@gmail.com>
2022-10-26 19:12:02 +0900

    카지노

---
+++ Else & Elif.py
@@ -0,0 +1,23 @@
+password_correct =False
+
+if password_correct:
+    print("Here is your money")
+else:
+    print("Wrong password")
+    
+## 패스워드가 맞으면 1번 실행해,
+## 아니면 2번 실행해
+
+
+## 이 두 가지를 합친게elif
+
+## 복권의 당첨번호를 예시
+
+winner = 20
+if winner>10:
+    print("winner is greater than 10")
+elif winner<10:
+    print("Winner is less than 10")
+else:
+    print("winner is 10")
+   
